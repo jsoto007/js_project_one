2bb10d2ed5c657769b4d071e681554f4eddb053c
Jose Soto <jsoto7087@gmail.com>
2021-07-09 02:40:52 -0400

    cannot save f & l in the document, cannot display on screem

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
-const URL = 'http://localhost3000/payroll'
+const URL = 'http://localhost3000/payroll';
 
 fetch(URL)
 .them(response => response.json())
 .then(data => {
 
 console.log(data);
+
+const ul = document.querySelector('#time_sheet')
+data.forEach(payroll => {
+    const timeSheet = `${payroll.id} ${payroll.rate} ${payroll.overTime}`;
+    const li = document.createElement('li')
+    li.textContent = timeSheet
+    ul.append(li);
+})
 
 })
 
