0c51613b93634dfe8008a6cc24dfbae3adb92b54
Jose Soto <jsoto7087@gmail.com>
2021-07-11 19:43:22 -0400

    added save, does not post

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 
+const url = 'http://localhost:3000/payroll'
 fetch('http://localhost:3000/payroll')
 .then(response => response.json())
 .then(data => {
@@ -7,13 +8,29 @@ console.log(data);
 
 const ul = document.querySelector('#time_sheet')
 data.forEach(payroll => {
-    const timeSheet = `${payroll.id} ${payroll.rate} ${payroll.overTime}`;
+    const timeSheet = `${payroll.id} ${payroll.rate} ${payroll.overTime} ${payroll.firstName} ${payroll.lastName} ${payroll.jobTitle}`;
     const li = document.createElement('li')
     li.textContent = timeSheet
     ul.append(li);
+
+
+    currentInfo = data
+    const information = data.info
+
 })
 
 })
+
+function postIndo(body) {
+    return fetch('http://localhost:3000/payroll', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify(body)
+    })
+    .then(res => res.json())
+  }
 
 
 
