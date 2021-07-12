8a76aa1864bcc34549e7d6f1598c6498cad4f584
Jose Soto <jsoto7087@gmail.com>
2021-07-11 22:21:59 -0400

    added submit button

--- services.js
+++ services.js
@@ -1,15 +1,17 @@
-const URL = 'http://localhost:3000/payroll'
+// const URL = 'http://localhost:3000/payroll'
 
 
 
 
-function postIndo(body) {
-    return fetch(URL, {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json'
-      },
-      body: JSON.stringify(body)
-    })
-    .then(res => res.json())
-  }
+// function postIndo(body) {
+//     return fetch(URL, {
+//       method: 'POST',
+//       headers: {
+//         'Accept': 'application/json'
+//       },
+//       body: JSON.stringify(body)
+//     })
+//     .then(res => res.json())
+//   }
+
+//   postIndo({innfo: "tesdting this stuff"})
